09f372e40ee5be80e56aee09b4cc601d59504f82
EOppedal <erlend.oppedal@gmail.com>
2024-09-13 14:50:46 +0200

    rootNamespace

--- Runtime/RuntimeSet.cs
+++ Runtime/RuntimeSet.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Collections.Generic;
 
-public class RuntimeSet<T> {
-    public readonly List<T> Items = new ();
-        
-    public event Action<int> OnCountChanged;
-        
-    public int Count => Items.Count;
-        
-    public void Add(T item) {
-        if (!Items.Contains(item)) Items.Add(item);
-        OnCountChanged?.Invoke(Items.Count);
-    }
-        
-    public void Remove(T item) {
-        if (Items.Contains(item)) Items.Remove(item);
-        OnCountChanged?.Invoke(Items.Count);
+namespace RuntimeSet {
+    public abstract class RuntimeSet<T> {
+        public readonly List<T> Items = new ();
+            
+        public event Action<int> OnCountChanged;
+            
+        public int Count => Items.Count;
+            
+        public void Add(T item) {
+            if (!Items.Contains(item)) Items.Add(item);
+            OnCountChanged?.Invoke(Items.Count);
+        }
+            
+        public void Remove(T item) {
+            if (Items.Contains(item)) Items.Remove(item);
+            OnCountChanged?.Invoke(Items.Count);
+        }
     }
 }
