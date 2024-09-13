b9c05deb4c761e9b71582383b20548fd396ad845
EOppedal <erlend.oppedal@gmail.com>
2024-09-13 14:57:33 +0200

    no root, Serializable

--- Runtime/RuntimeSet.cs
+++ Runtime/RuntimeSet.cs
@@ -1,22 +1,21 @@
 using System;
 using System.Collections.Generic;
 
-namespace RuntimeSet {
-    public abstract class RuntimeSet<T> {
-        public readonly List<T> Items = new ();
-            
-        public event Action<int> OnCountChanged;
-            
-        public int Count => Items.Count;
-            
-        public void Add(T item) {
-            if (!Items.Contains(item)) Items.Add(item);
-            OnCountChanged?.Invoke(Items.Count);
-        }
-            
-        public void Remove(T item) {
-            if (Items.Contains(item)) Items.Remove(item);
-            OnCountChanged?.Invoke(Items.Count);
-        }
+[Serializable]
+public class RuntimeSet<T> {
+    public List<T> items = new ();
+        
+    public event Action<int> OnCountChanged;
+        
+    public int Count => items.Count;
+        
+    public void Add(T item) {
+        if (!items.Contains(item)) items.Add(item);
+        OnCountChanged?.Invoke(items.Count);
+    }
+        
+    public void Remove(T item) {
+        if (items.Contains(item)) items.Remove(item);
+        OnCountChanged?.Invoke(items.Count);
     }
 }
